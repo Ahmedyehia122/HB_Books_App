504a3aec74408d8c42c7432f08c929cec738f770
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-27 21:06:00 +0300

    implement fetch remote home remote data source (fetchbookslistview)

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,3 +1,6 @@
+import 'package:bookly_app/core/class/api_service.dart';
+import 'package:bookly_app/core/constants/end_points.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -6,10 +9,17 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl({required this.apiService});
   @override
-  Future<List<BookEntity>> fetchBestSellerListView() {
-    // TODO: implement fetchBestSellerListView
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchBestSellerListView() async {
+    var data = await apiService.get(url: EndPoints.fetchBooksEndPoint);
+    List<BookEntity> books = [];
+    for (var book in data) {
+      books.add(BookModel.fromJson(book));
+    }
+    return books;
   }
 
   @override
