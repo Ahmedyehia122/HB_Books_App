e09a402283712fb1f657b95354ea0af462892df7
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-28 00:29:00 +0300

    generate book entity adapter

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,8 +1,17 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String? image;
+  @HiveField(1)
   final String? bookTitle;
+  @HiveField(2)
   final String? authorName;
+  @HiveField(3)
   final num? price;
+  @HiveField(4)
   final int? reviews;
 
   BookEntity(
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:bookly_app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 
 void main() async {
-  await Hive.initFlutter();
   runApp(const BooklyApp());
 }
