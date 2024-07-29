096762ce2dbaf1454fc2dd3da3f008f75720dd3a
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-29 13:50:00 +0300

    create serverce locator

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,13 @@
 import 'package:bookly_app/core/constants/fonts.dart';
 import 'package:bookly_app/core/constants/routes.dart';
+import 'package:bookly_app/core/functions/service_locator.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/domain/use_cases/fetch_best_seller_list_view_use_case.dart';
+import 'package:bookly_app/features/home/domain/use_cases/fetch_books_list_view_use_case.dart';
+import 'package:bookly_app/features/home/presentation/manager/best_seller_list_view_cubit/best_seller_list_view_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/books_list_view_cubit/books_list_view_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BooklyApp extends StatelessWidget {
@@ -12,13 +19,31 @@ class BooklyApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          fontFamily: AppFonts.montserrat,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => BooksListViewCubit(
+              FetchBooksListViewUseCase(
+                homeRepo: getIt.get<HomeRepoImpl>(),
+              ),
+            ),
+          ),
+          BlocProvider(
+            create: (context) => BestSellerListViewCubit(
+              FetchBestSellerListViewUseCase(
+                homeRepo: getIt.get<HomeRepoImpl>(),
+              ),
+            ),
+          ),
+        ],
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            fontFamily: AppFonts.montserrat,
+          ),
+          routerConfig: AppRoutes.router,
         ),
-        routerConfig: AppRoutes.router,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:bookly_app/app.dart';
 import 'package:bookly_app/core/constants/hive_box.dart';
+import 'package:bookly_app/core/functions/service_locator.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/simble_bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+  Bloc.observer = SimbleBlocObserver();
+  setupServiceLocator();
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   Hive.openBox<BookEntity>(HiveBox.kBooksListViewBox);
