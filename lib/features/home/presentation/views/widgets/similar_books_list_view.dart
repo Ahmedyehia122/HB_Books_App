a1e1cc62c88dcad11563a13007bb838560db60b7
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-30 14:23:56 +0300

    display similar books list view

--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,14 +1,15 @@
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SimilarBooksListView extends StatelessWidget {
-  const SimilarBooksListView({super.key});
+  const SimilarBooksListView({super.key, required this.books});
+
+  final List<BookEntity> books;
 
   @override
   Widget build(BuildContext context) {
-    String testImage =
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtiJKuQkGMitVsqqDyAppVphB6JCTlpEl8_tytAzIVrG-z5NgLGQcQb8uCeQDF7ueRI0w&usqp=CAU';
     return Padding(
       padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
       child: SizedBox(
@@ -16,12 +17,12 @@ class SimilarBooksListView extends StatelessWidget {
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           physics: const BouncingScrollPhysics(),
-          itemCount: 10,
+          itemCount: books.length,
           itemBuilder: (BuildContext context, int index) {
             return Padding(
               padding: EdgeInsets.only(right: 10.w),
               child: CustomBookImage(
-                imageUrl: testImage,
+                imageUrl: books[index].image ?? '',
               ),
             );
           },
--- lib/features/home/presentation/views/widgets/similar_books_section.dart
+++ lib/features/home/presentation/views/widgets/similar_books_section.dart
@@ -1,6 +1,10 @@
 import 'package:bookly_app/core/constants/styles.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/books_list_view_cubit/books_list_view_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksSection extends StatelessWidget {
   const SimilarBooksSection({super.key});
@@ -17,8 +21,29 @@ class SimilarBooksSection extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
         ),
-        const SimilarBooksListView(),
+        const SimilarBooksListViewBlocBuilder(),
       ],
+    );
+  }
+}
+
+class SimilarBooksListViewBlocBuilder extends StatelessWidget {
+  const SimilarBooksListViewBlocBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BooksListViewCubit, BooksListViewState>(
+      builder: (context, state) {
+        if (state is BooksListViewSuccess) {
+          return SimilarBooksListView(books: state.books);
+        } else if (state is BooksListViewFailure) {
+          return CustomErrorWidget(errorMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
